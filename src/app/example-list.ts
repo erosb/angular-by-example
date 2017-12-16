1ecd528e8e3e9713858a5f7055c15e3cb4c23fbf
Bence Erős <ebence88@gmail.com>
2017-12-16 22:44:58 +0100

    changing ExampleComponent to display all source files of the example

--- src/app/example-list.ts
+++ src/app/example-list.ts
@@ -1,10 +1,18 @@
 import {HelloWorldComponent} from "./hello-world/hello-world.component";
 import {EventHandlingComponent} from "./event-handling/event-handling.component";
+import {Type} from "@angular/core";
 
-export const exampleList = [
+export interface ExampleDefinition {
+
+  title: string,
+  files: string[],
+  component: Type<any>
+}
+
+export const exampleList: ExampleDefinition[] = [
   {
     title: "Hello world !",
-    files: ["example.component.ts"],
+    files: ["app/hello-world/hello-world.component.ts"],
     component: HelloWorldComponent
   },
   {
--- src/app/example.component.ts
+++ src/app/example.component.ts
@@ -1,6 +1,17 @@
 import {AfterViewInit, Component, ComponentFactoryResolver, Directive, Input, ViewChild, ViewContainerRef} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import * as Prism from "prismjs";
+import {ExampleDefinition} from "./example-list";
+
+interface SourceFile {
+
+  fileName: string;
+
+  relPath: string;
+
+  highlightedText: string
+
+}
 
 @Directive({
   selector: "[running-example]"
@@ -19,25 +30,32 @@ export class ExampleDisplay {
 })
 export class ExampleComponent implements AfterViewInit {
 
-  @Input() example;
+  @Input() example: ExampleDefinition;
 
   @ViewChild(ExampleDisplay) exampleDisplay;
 
   public source: string;
 
+  public sourceFiles: SourceFile[] = [];
+
   public ngAfterViewInit(): void {
     setTimeout(() => {
-      console.log(this.exampleDisplay)
-
       const componentFactory = this.componentFactoryResolver.resolveComponentFactory(this.example.component);
       const viewContainerRef = this.exampleDisplay.viewContainerRef;
       viewContainerRef.clear();
-
       const componentRef = viewContainerRef.createComponent(componentFactory);
     });
-    this.httpClient.get("/app/event-handling/event-handling.component.ts", {responseType: "text"})
+    this.example.files.forEach(this.fetchSourceFile.bind(this));
+  }
+
+  private fetchSourceFile(fileName: string): void {
+    this.httpClient.get(fileName, {responseType: "text"})
       .subscribe(resp => {
-        this.source = Prism.highlight(resp, Prism.languages.javascript);
+        this.sourceFiles.push({
+          relPath: fileName,
+          fileName: fileName.substring(fileName.lastIndexOf("/") + 1),
+          highlightedText: Prism.highlight(resp, Prism.languages.javascript)
+        });
       });
   }
 
